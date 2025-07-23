5b80beb7d2edca99bcd62acbe2070b34a8bcba83
Aditi Naik <aditinaik277@gmail.com>
2025-07-23 18:52:50 +0530

    Minor changes

--- my-app/src/client.jsx
+++ my-app/src/client.jsx
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://keoheimljchataqydfya.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlb2hlaW1samNoYXRhcXlkZnlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzNDgyMTMsImV4cCI6MjA1ODkyNDIxM30.RY77O-V815C0VR3VOV7HX2f4SAacRye_RsveZVyPcaA'
+//secure in .env as done in backend, this is only for referral while initial testing
+const supabaseUrl = ''
+const supabaseKey = ''
 export const supabase = createClient(supabaseUrl, supabaseKey)
